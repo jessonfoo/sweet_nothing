c1c839f287371e69f4177391e4222b6adf6a87d8
Brooks Roley <brooksroley@gmail.com>
2014-08-01 14:13:46 -0700

    Please enter the commit message for your changes. Lines starting
    TRYING  to merge
    X

--- source/sweet_nothings/db/001_messages_table.rb
+++
@@ -1,8 +0,0 @@
-class GenericMessagesTable < ActiveRecord::Migration
-  def change
-    create_table :generic_messages do |table|
-      table.string  :message
-      table.timestamps
-    end
-  end
-end
