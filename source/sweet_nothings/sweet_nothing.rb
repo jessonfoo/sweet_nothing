6a1c2607431bbb1375ea23813b34bd9450bee648
Jessonfoo <jessonfoo@gmail.com>
2014-08-01 13:54:58 -0700

    update

--- source/sweet_nothings/sweet_nothing.rb
+++ source/sweet_nothings/sweet_nothing.rb
@@ -6,14 +6,14 @@ class sweet_nothing
 	auth_token = "YYYYYYYYYYYYYYYYYY"
 
 	public
-
+	number = 
 
 
 	@client = Twilio::REST::Client.new account_sid, auth_token 
 
 	@client.account.messages.create({
 		from: '+19099920609',   
-		to: '+19495255124',
+		to: "+#{number}",
 		body: "#{message}"
 		})
 
